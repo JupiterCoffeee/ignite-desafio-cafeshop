d56213d9c5b63edc28d5e8580fa51127c016d68b
JupiterCoffeee <jvcfialho@gmail.com>
2023-11-28 09:54:36 +0000

    App router and theme provider configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Router } from "./Router";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/theme";
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from "styled-components";
+
 export function App() {
 
   return (
-    <>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
