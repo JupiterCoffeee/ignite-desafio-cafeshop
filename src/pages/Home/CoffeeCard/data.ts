e07a8fd1c1b18ff92ea5107be92c34612b54f17f
JupiterCoffeee <jvcfialho@gmail.com>
2023-12-07 14:28:06 +0000

    Coffees id's are now strings

--- src/pages/Home/CoffeeCard/data.ts
+++ src/pages/Home/CoffeeCard/data.ts
@@ -15,17 +15,18 @@ import Mochaccino from '../../../assets/Mochaccino.png'
 
 export const data = [
     {
-        id: 1,
+        id: '1',
         title: 'Expresso Tradicional',
         description: 'O tradicional café feito com água quente e grãos moídos',
         banner: `${Expresso}`,
         tag: [
             'TRADICIONAL',
         ],
-        price: '9.90'
+        price: '9.90',
+        amount: 0
     },
     {
-        id: 2,
+        id: '2',
         title: 'Expresso Americano',
         description: 'Expresso diluído, menos intenso que o tradicional',
         banner: `${Americano}`,
@@ -35,7 +36,7 @@ export const data = [
         price: '9.90'
     },
     {
-        id: 3,
+        id: '3',
         title: 'Expresso Cremoso',
         description: 'Café expresso tradicional com espuma cremosa',
         banner: `${ExpressoCremoso}`,
@@ -45,7 +46,7 @@ export const data = [
         price: '9.90'
     },
     {
-        id: 4,
+        id: '4',
         title: 'Expresso Gelado',
         description: 'Bebida preparada com café expresso e cubos de gelo',
         banner: `${CafeGelado}`,
@@ -56,7 +57,7 @@ export const data = [
         price: '9.90'
     },
     {
-        id: 5,
+        id: '5',
         title: 'Café com Leite',
         description: 'Meio a meio de expresso tradicional com leite vaporizado',
         banner: `${CafeComLeite}`,
@@ -67,7 +68,7 @@ export const data = [
         price: '9.90'
     },
     {
-        id: 6,
+        id: '6',
         title: 'Latte',
         description: 'Uma dose de café expresso com o dobro de leite e espuma cremosa',
         banner: `${Latte}`,
@@ -78,7 +79,7 @@ export const data = [
         price: '9.90'
     },
     {
-        id: 7,
+        id: '7',
         title: 'Capuccino',
         description: 'Bebida com canela feita de doses iguais de café, leite e espuma',
         banner: `${Capuccino}`,
@@ -89,7 +90,7 @@ export const data = [
         price: '9.90'
     },
     {
-        id: 8,
+        id: '8',
         title: 'Macchiato',
         description: 'Café expresso misturado com um pouco de leite quente e espuma',
         banner: `${Macchiato}`,
@@ -100,7 +101,7 @@ export const data = [
         price: '9.90'
     },
     {
-        id: 9,
+        id: '9',
         title: 'Mocaccino',
         description: 'Café expresso com calda de chocolate, pouco leite e espuma',
         banner: `${Mochaccino}`,
@@ -111,7 +112,7 @@ export const data = [
         price: '9.90'
     },
     {
-        id: 10,
+        id: '10',
         title: 'Chocolate Quente',
         description: 'Bebida feita com chocolate dissolvido no leite quente e café',
         banner: `${ChocolateQuente}`,
@@ -122,7 +123,7 @@ export const data = [
         price: '9.90'
     },
     {
-        id: 11,
+        id: '11',
         title: 'Cubano',
         description: 'Drink gelado de café expresso com rum, creme de leite e hortelã',
         banner: `${Cubano}`,
@@ -134,7 +135,7 @@ export const data = [
         price: '9.90'
     },
     {
-        id: 12,
+        id: '12',
         title: 'Havaiano',
         description: 'Bebida adocicada preparada com café e leite de coco',
         banner: `${Havaiano}`,
@@ -144,7 +145,7 @@ export const data = [
         price: '9.90'
     },
     {
-        id: 13,
+        id: '13',
         title: 'Árabe',
         description: 'Bebida preparada com grãos de café árabe e especiarias',
         banner: `${Arabe}`,
@@ -154,7 +155,7 @@ export const data = [
         price: '9.90'
     },
     {
-        id: 14,
+        id: '14',
         title: 'Irlândes',
         description: 'Bebida a base de café, uísque irlandês, açúcar e chantilly',
         banner: `${Irlandes}`,
