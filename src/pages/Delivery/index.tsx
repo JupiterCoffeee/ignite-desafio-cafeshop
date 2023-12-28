27174890da8f0a2f393c06fa46eeedf22362ec6f
JupiterCoffeee <jvcfialho@gmail.com>
2023-12-28 16:46:17 +0000

    Started Delivery Page

--- src/pages/Delivery/index.tsx
+++ src/pages/Delivery/index.tsx
@@ -1,6 +1,43 @@
+import { Info, MapPin } from "phosphor-react";
+import 
+{ 
+    DeliveryContainer, 
+    DeliveryInfoDiv, 
+    DeliveryTitleDiv
+} from "./style";
+
 export function Delivery() {
     return (
-        <>
-        </>
+        <DeliveryContainer>
+            <DeliveryTitleDiv>
+                <h2>Uhu! Pedido confirmado</h2>
+                <h3>Agora é só aguardar que logo o café chegará até você</h3>
+            </DeliveryTitleDiv>
+            <DeliveryInfoDiv>
+                <div>
+                    <Info>
+                        <span><MapPin weight="fill"/></span>
+                        <div>
+                            <p>Entrega em <strong></strong></p>
+                            <p>São Paulo - São Paulo, SP</p>
+                        </div>
+                    </Info>
+                    <Info>
+                        <span><MapPin weight="fill"/></span>
+                        <div>
+                            <p>Previsão de entrega</p>
+                            <strong>20 min - 30 min</strong>
+                        </div>
+                    </Info>
+                    <Info>
+                        <span><MapPin weight="fill"/></span>
+                        <div>
+                            <p>Entrega em</p>
+                            <strong></strong>
+                        </div>
+                    </Info>
+                </div>
+            </DeliveryInfoDiv>
+        </DeliveryContainer>
     )
 }
---
+++ src/pages/Delivery/style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const DeliveryContainer = styled.div`
+
+`;
+
+export const DeliveryTitleDiv = styled.div`
+
+`;
+
+export const DeliveryInfoDiv = styled.div`
+
+`;
+
+export const Info = styled.div`
+
+`;
